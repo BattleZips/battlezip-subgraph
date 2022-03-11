2cf340491582a58a62723070659c65be40d9cceb
Ian-Bright <ianbrighton1@gmail.com>
2022-03-11 07:43:54 -0700

    add different network support

--- src/mappings/BattleshipGame.ts
+++ src/mappings/BattleshipGame.ts
@@ -1,36 +1,46 @@
-import { Joined, Started, Shot, Won, Collected } from '../../generated/BattleshipGame/BattleshipGame';
+import { Joined, Started, Shot, Won, Report } from '../../generated/BattleshipGame/BattleshipGame';
+import { BigInt } from '@graphprotocol/graph-ts';
 import { BIG_INT_ONE } from '../constants';
 import { ensureBattleShipGame } from '../entities/Currency';
 import { ensureShot } from '../entities/Shot';
-import { log } from '@graphprotocol/graph-ts';
 
 export function handleGameStarted(event: Started): void {
-    let game = ensureBattleShipGame(event.params._nonce.toString());
-    game.save();
+  let game = ensureBattleShipGame(event.params._nonce.toString());
+  game.startedBy = event.params._by;
+  game.save();
 }
 
 export function handleGameJoined(event: Joined): void {
-    let game = ensureBattleShipGame(event.params._nonce.toString());
-    game.status = 'JOINED';
-    game.save();
+  let game = ensureBattleShipGame(event.params._nonce.toString());
+  game.joinedBy = event.params._by;
+  game.status = 'JOINED';
+  game.save();
+}
+
+export function handleReport(event: Report): void {
+  let game = ensureBattleShipGame(event.params._game.toString());
+  let shot = ensureShot(game.id.toString().concat('-').concat(game.totalShots.toString()));
+  shot.hit = event.params.hit;
+  shot.save();
 }
 
 export function handleShot(event: Shot): void {
-    let shot = ensureShot(event.params._game.toString().concat('-').concat(event.params._turn.toString()));
-    let game = ensureBattleShipGame(event.params._game.toString());
-    shot.game = game.id;
-    shot.hit = event.params._hit;
-    shot.turn = event.params._turn;
-    game.totalShots = game.totalShots.plus(BIG_INT_ONE);
-    shot.save();
-    game.save();
+  let gameId = event.params._game.toString();
+  let game = ensureBattleShipGame(gameId);
+  let totalShots = (game.totalShots = game.totalShots.plus(BIG_INT_ONE));
+  let shot = ensureShot(event.params._game.toString().concat('-').concat(totalShots.toString()));
+  game.totalShots = totalShots;
+  shot.game = gameId;
+  shot.turn = totalShots;
+  shot.x = BigInt.fromI32(event.params._x);
+  shot.y = BigInt.fromI32(event.params._y);
+  shot.save();
+  game.save();
 }
 
 export function handleGameWon(event: Won): void {
-    let game = ensureBattleShipGame(event.params._nonce.toString());
-    game.status = 'OVER';
-    game.winner = event.params._winner;
-    game.save();
+  let game = ensureBattleShipGame(event.params._nonce.toString());
+  game.status = 'OVER';
+  game.winner = event.params._winner;
+  game.save();
 }
-
-export function handleRewardCollected(event: Collected): void { }
